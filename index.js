1bfa5efba6f51067c9f3859e96e9fd667a4c7a25
Wei Guangcheng <twksos@qq.com>
2017-08-28 22:53:38 -0400

    init

---
+++ index.js
@@ -0,0 +1,9 @@
+// credit to Rob W          https://stackoverflow.com/users/938089/rob-w
+//       and Community      https://stackoverflow.com/users/-1/community
+// https://stackoverflow.com/questions/9515704/insert-code-into-the-page-context-using-a-content-script
+var s = document.createElement('script');
+s.src = chrome.extension.getURL('beep.js');
+s.onload = function() {
+    // this.remove();
+};
+(document.head || document.documentElement).appendChild(s);
